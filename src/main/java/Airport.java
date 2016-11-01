aedd5e9b8a994bb1eb358c67f6baa09f4c89f2fa
Hanif Fakira <fakira.work@gmail.com>
2016-11-01 14:10:31 +0000

    US1 airport construct takes plane as arg

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -1,4 +1,6 @@
 
 public class Airport {
-	
+	public Airport(Plane plane){
+		
+	}
 }
--- src/test/java/TestAirport.java
+++ src/test/java/TestAirport.java
@@ -1,9 +1,15 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.mockito.Mock;
 
 public class TestAirport {
+	 @Mock
+	 Plane plane;
+	
 	 @Test
 	 public void canCreateObject() {
-		 new Airport();
+		 new Airport(plane);
 	 }
+	 
+	 
 }
