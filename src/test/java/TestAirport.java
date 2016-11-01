0aee210046d4fde52685898a03a1e24e7af22a01
Hanif Fakira <fakira.work@gmail.com>
2016-11-01 13:46:02 +0000

    initialise tests, first test

---
+++ src/test/java/TestAirport.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestAirport {
+	 @Test
+	 public void canCreateObject() {
+		 new Airport();
+	 }
+}
---
+++ src/test/java/TestFeature.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+ 
+public class TestFeature {
+	/*
+	 * As an air traffic controller 
+	 * So I can get passengers to a destination 
+	 * I want to instruct a plane to land at an airport 
+	 * 		and confirm that it has landed 
+	 */
+//    @Test 
+//    public void testUserStory1() {
+//    	  Plane plane = new Plane();
+//        Airport airport = new Airport(plane);
+//        airport.instruct_to_land();
+//        assertEquals(1, airport.getHanger().length);
+//        assertArrayEquals(plane, airport.getHanger()[0]);
+//        assertEquals(true, plane.at_airport());
+//    }
+}
