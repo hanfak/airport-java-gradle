99415a971be9d04fdbd696dc76c64bbb0f7712fb
Hanif Fakira <fakira.work@gmail.com>
2016-11-30 09:42:10 +0000

    US3 fixed feature tests

--- src/test/java/TestFeature.java
+++ src/test/java/TestFeature.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
  
@@ -23,26 +25,30 @@ public class TestFeature {
     So I can get passengers on the way to their destination 
     I want to instruct a plane to take off from an airport
      and confirm that it is no longer in the airport*/
-//    @Test
-//    public void UserStory2() throws PlaneException, AirportException {
-//    	Plane plane = new Plane();
-//    	Airport airport = new Airport();
-//    	airport.instructToLand(plane);
-//    	airport.instructToDepart(plane);
-//    	assertFalse(airport.getHanger().contains(plane));
-//    	assertFalse(plane.atAirport());
-//    }
-//    
+    @Test
+    public void UserStory2() throws PlaneException, AirportException {
+    	Plane plane = new Plane();
+    	Weather mockWeather = mock(Weather.class);
+    	Airport airport = new Airport(mockWeather);
+    	when(mockWeather.isStormy()).thenReturn(false);
+    	airport.instructToLand(plane);
+    	airport.instructToDepart(plane);
+    	assertFalse(airport.getHanger().contains(plane));
+    	assertFalse(plane.atAirport());
+    }
+    
     /*As an air traffic controller 
     To ensure safety 
     I want to prevent takeoff when weather is stormy*/
-//    @Test
-//    public void UserStory3() throws PlaneException {
-//    	Plane plane = new Plane();
-//    	Airport airport = new Airport();
-//    	airport.instructToLand(plane);
-//    	airport.instructToDepart(plane);
-//    	assertTrue(airport.getHanger().contains(plane));
-//    	assertTrue(plane.atAirport());
-//    }
+    @Test
+    public void UserStory3() throws PlaneException {
+    	Plane plane = new Plane();
+    	Weather mockWeather = mock(Weather.class);
+    	Airport airport = new Airport(mockWeather);
+    	when(mockWeather.isStormy()).thenReturn(true);
+    	airport.instructToLand(plane);
+    	try { airport.instructToDepart(plane); } catch (AirportException e) {}
+    	assertTrue(airport.getHanger().contains(plane));
+    	assertTrue(plane.atAirport());
+    }
 }
